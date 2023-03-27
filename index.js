4311806733da245c48b3825bb6d93a7c797b7512
oztuzelosman <oztuzelosman@gmail.com>
2023-03-27 09:33:12 +0000

    Add router func via url

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+//////////////////////
+///////FILE
+
 //### SYNCHRONOUS WAY
 
 /* const fs = require("fs");
@@ -14,23 +17,59 @@ console.log(written);
  */
 
 //### ASYNCHRONOUS WAY
+/* 
 
 const fs = require("fs");
 
-fs.readFile("./txt/startttt.txt", "utf-8", (err, data1) => {
-  if (err) {
-    return console.log("ERROR!!");
-  }
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  if (err) return console.log("ERROR!!");
+
   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+    if (err) return console.log("ERROR2!!");
     console.log(data2);
+
     fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
       console.log(data3);
+
       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
         console.log("your file has been written.");
-        if ((!data2 && data3) || err) {
-          console.log("ERRORRR!!!");
-        }
       });
     });
   });
 });
+ */
+
+/////////////////////////////
+/////////SERVER
+
+const http = require("http");
+const url = require("url");
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  if (pathName === "/overview") {
+    res.end("Hello from server");
+  } else if (pathName === "/product") {
+    res.end("Hello from node");
+  } else {
+    res.end("Hey NODE!!");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening");
+});
+
+/* const http = require("http");
+const url = require("url");
+
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+  res.end("hello from server!");
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("listening");
+});
+ */
