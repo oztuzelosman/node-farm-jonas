3ebe37dcf7a8221473bdc50d4daf10a4a63f66bc
oztuzelosman <oztuzelosman@gmail.com>
2023-03-29 11:24:12 +0000

    finish http module

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-const http = require('http')
-
-const server = http.createServer((req, res) => {
-    if(req.url === "/" || req.url=== "/home"){
-        res.end('Welcome to my serveer')
-    } else if(req.url === "/about"){
-        res.end('History')
-    } else {
-        res.end("We can not find what you are looking for")
-    }
-})
-
-server.listen(5000)
+console.log("eemt");
